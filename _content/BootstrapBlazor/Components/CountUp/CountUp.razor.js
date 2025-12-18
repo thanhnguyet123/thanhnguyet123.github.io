622510a77d5cc8f43c3f78f42c6cbbc4555312a5
Lê Phạm Thanh Nguyệt <163025020+thanhnguyet123@users.noreply.github.com>
2025-12-18 17:29:14 +0000

    deploy 1

---
+++ _content/BootstrapBlazor/Components/CountUp/CountUp.razor.js
@@ -0,0 +1,14 @@
+﻿import { CountUp } from "../../lib/countUp/countUp.min.js?v=8.0.0"
+import Data from "../../modules/data.js?v=8.0.0"
+
+export function init(id, invoke, val, callback, option) {
+    option = option || {}
+    if (callback !== null) {
+        option.onCompleteCallback = () => {
+            invoke.invokeMethodAsync(callback)
+        }
+    }
+
+    const countUp = new CountUp(id, val, option)
+    countUp.start()
+}
---
+++ _content/BootstrapBlazor/Components/ImageViewer/ImageViewer.razor.js
@@ -0,0 +1,58 @@
+﻿import Data from "../../modules/data.js?v=8.0.0"
+import EventHandler from "../../modules/event-handler.js?v=8.0.0"
+
+const setListeners = viewer => {
+    if (viewer.prevList && viewer.prevList.length > 0) {
+        EventHandler.on(viewer.img, 'click', () => {
+            if (!viewer.previewer) {
+                viewer.previewer = Data.get(viewer.previewerId)
+            }
+            if (viewer.previewer) {
+                viewer.previewer.viewer.show()
+            }
+        })
+    }
+}
+
+export function init(id, url, preList) {
+    const el = document.getElementById(id)
+    if (el === null) {
+        return
+    }
+    const viewer = {
+        element: el,
+        img: el.querySelector('img'),
+        async: el.getAttribute('data-bb-async'),
+        prevList: preList || [],
+        previewerId: el.getAttribute('data-bb-previewer-id')
+    }
+    if (url) {
+        viewer.prevList.push(url)
+    }
+    Data.set(id, viewer)
+
+    if (viewer.img && viewer.async) {
+        viewer.img.setAttribute('src', url)
+    }
+
+    setListeners(viewer)
+}
+
+export function update(id, prevList) {
+    const viewer = Data.get(id)
+    if (viewer.img) {
+        EventHandler.off(viewer.img, 'click')
+    }
+
+    viewer.prevList = prevList
+    setListeners(viewer)
+}
+
+export function dispose(id) {
+    const viewer = Data.get(id)
+    Data.remove(id)
+
+    if (viewer.img) {
+        EventHandler.off(viewer.img, 'click')
+    }
+}
